cedb7f43980fe6f1710b08ffe590ffcf279d1b37
hirahul0011 <ra_10001@yahoo.co.in>
2023-06-17 16:22:52 +0530

    Protected and Public Examples(Pro and Pub should be in Package 1 and AS3 and AS4 should be in Package 2)

---
+++ AccessSpecifiers3.java
@@ -0,0 +1,15 @@
+package pack2;
+
+import pack1.ProAccessSpecifiers;
+
+public class AccessSpecifiers3 extends ProAccessSpecifiers{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AccessSpecifiers3 obj = new AccessSpecifiers3 ();   
+	       obj.display();  
+
+
+	}
+
+}
---
+++ AccessSpecifiers4.java
@@ -0,0 +1,14 @@
+package pack2;
+
+import pack1.*;
+
+public class AccessSpecifiers4 extends PubAccessSpecifiers{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AccessSpecifiers4 obj = new AccessSpecifiers4(); 
+        obj.display();  
+
+	}
+
+}
